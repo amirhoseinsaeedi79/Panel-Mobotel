6cd92ca3b127ff7ea37761415dfbf26ff07fef9b
amirhosein79 <amirhosein.saeedi790109@gmail.com>
2023-10-12 23:29:20 +0330

    start topbar

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    container:{
+      center: true,
+    },
+
+    extend: {
+      colors:{
+        blue:"#068fff", 
+      }
+    
+
+    },
+  },
+  plugins: [],
+}
